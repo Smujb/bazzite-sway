fd6fd2aaa6d2154ca6892a7b57b4094bfe9e5e00
SmuJayB <smujamesb@gmail.com>
2026-01-01 12:29:39 +0000

    Remove some KDE utilities

--- build_files/build.sh
+++ build_files/build.sh
@@ -40,46 +40,18 @@ dnf5 install -y liquidctl coolercontrol
 # install kitty (preferred terminal) and zsh. don't change preferred shell so as not to break anything
 dnf5 install -y kitty zsh
 
+### --- replace some KDE utilities --- ###
+
 # remove some KDE stuff
 dnf5 remove -y plasma-desktop xwaylandvideobridge
 
-# Comment out attempt to install everything needed by sway; I'll try Fedora defaults for now
-# launcher
-# dnf5 install -y rofi-wayland wofi
+# Replace kwrite and kate with mousepad
+dnf5 remove -y kwrite kate && dnf5 install -y mousepad
 
-# environment
-# dnf5 install -y fzf tuned-ppd xorg-x11-server-Xwayland headsetcontrol mediainfo polkit xfce-polkit fprintd-pam xdg-user-dirs dbus-tools dbus-daemon wl-clipboard pavucontrol playerctl qt5-qtwayland qt6-qtwayland vulkan-validation-layers vulkan-tools google-noto-emoji-fonts gnome-disk-utility ddcutil openssl vim just alsa-firmware p7zip distrobox steam-devices
+# remove kwallet and polkit-kde for lxqt-policykit
+dnf5 remove -y kwallet polkit-kde && dnf5 install -y lxqt-policykit
 
-# sound
-# dnf5 install -y wireplumber pipewire pamixer pulseaudio-utils
-
-# networking
-# dnf5 install -y network-manager-applet NetworkManager-openvpn NetworkManager-openvpn-gnome NetworkManager-openconnect NetworkManager-openconnect-gnome bluez bluez-tools blueman firewall-config
-
-# file manager
-# dnf5 install -y thunar thunar-archive-plugin thunar-volman xarchiver imv p7zip gvfs-mtp gvfs-gphoto2 gvfs-smb gvfs-nfs gvfs-fuse gvfs-archive android-tools
-
-# screenshot
-# dnf5 install -y slurp grim
-
-# display
-# dnf5 install -y wlr-randr wlsunset brightnessctl kanshi
-
-# notifications
-# dnf5 install -y dunst
-
-# theme and GUI
-# dnf5 install -y fontawesome-fonts-all gnome-themes-extra gnome-icon-theme paper-icon-theme breeze-icon-theme papirus-icon-theme
-
-# sway stuff
-# dnf5 install -y sway swaylock swayidle foot waybar
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# I prefer Dolphin to other file managers so no need to replace that for example
 
 #### Example for enabling a System Unit File
 
