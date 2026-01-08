fe9c1e05c90a8ea2501dcd7756f5545f83881c5c
SmuJayB <smujamesb@gmail.com>
2026-01-08 18:13:57 +0000

    Install weak deps too

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,7 +33,7 @@ dnf5 remove -y fcitx*
 dnf5 install -y sway-config-fedora
 
 # Setup SDDM as it was removed earlier
-dnf5 install -y sddm sddm-themes sddm-wayland-sway
+dnf5 install -y sddm sddm-themes sddm-wayland-sway --setopt install_weak_deps=true
 
 # install systemd setup
 dnf5 install -y sway-systemd
