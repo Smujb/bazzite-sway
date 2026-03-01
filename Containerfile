69eb8e8202721be121831fffd40ca507af79078a
JayB <smujamesb@gmail.com>
2026-03-01 07:52:13 +0000

    Build swaywsr and still on the image

--- Containerfile
+++ Containerfile
@@ -47,6 +47,12 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
+    /ctx/20-build-additional-packages.sh
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
     /ctx/99-image-info.sh
     
 ### LINTING
---
+++ build_files/20-build-additional-packages.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# State that we are building additional packages
+echo "::group::Executing build-additional-packages"
+trap 'echo "::endgroup::"' EXIT
+
+### --- swaywsr --- ###
+SWAYWSR_LOCATION="/usr/share/swaywsr/tmp"
+
+# Install dependencies
+dnf5 -y install cargo
+rm -rf /root # Installing cargo seems to create a phantom /root directory
+
+# Create a temporary directory for storing build files and download the repo
+mkdir -p $SWAYWSR_LOCATION && cd $SWAYWSR_LOCATION
+git clone https://github.com/pedroscaff/swaywsr.git
+
+# Build swaywsr
+cd swaywsr
+cargo build --release
+
+# Copy it out of the build directory
+cp $SWAYWSR_LOCATION/swaywsr/target/release/swaywsr /usr/bin/swaywsr
+
+# Cleanup
+cd /
+dnf5 -y remove cargo
+rm -rf $SWAYWSR_LOCATION
+rm -rf /root/.cargo
+
+### --- still --- ###
+STILL_LOCATION="/usr/share/still/tmp"
+
+# Install dependencies
+dnf5 -y install meson pixman-devel wayland-devel wayland-protocols-devel
+
+# Create a temporary directory for storing build files and download the repo
+mkdir -p $STILL_LOCATION && cd $STILL_LOCATION
+git clone https://github.com/faergeek/still.git
+
+# Build still
+cd still
+meson setup --buildtype release build
+ninja -C build
+
+# Copy it out of the build directory
+cp $STILL_LOCATION/still/build/still /usr/bin/still
+
+# Cleanup
+cd /
+dnf5 -y remove meson pixman-devel wayland-devel wayland-protocols-devel
+rm -rf $STILL_LOCATION
